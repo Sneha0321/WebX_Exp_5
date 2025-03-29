7bd559b963d7f4e4fd8e7118133fe254da1b888c
Sneha0321 <142154919+Sneha0321@users.noreply.github.com>
2025-03-30 00:19:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+#app.py
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/user', methods=['GET'])
+def user():
+    username = request.args.get('username', 'Guest')
+    return render_template('user.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
